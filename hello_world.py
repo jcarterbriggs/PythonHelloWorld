6425f670729203eb82227a63d69df2361615c83e
jcarterbriggs <jcarterbriggs@gmail.com>
2020-11-28 10:27:13 -0500

    commit

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,7 @@
-print("Hello Carter")
+#!/usr/bin/python
+import pandas as pd
+
+df = pd.read_csv('amazon-orders.csv')
+df = df.fillna(0)
+df["Total Charged"] = df["Total Charged"].str.replace('$','').astype(float)
+print(df.head())
